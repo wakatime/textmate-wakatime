426cbcf571ebef7ff47445a3d76ed02c8884d814
Alan Hamlett <alan.hamlett@gmail.com>
2015-04-07 12:26:43 -0700

    add methods to header file

--- NSWindow+KKSwizzle.h
+++ NSWindow+KKSwizzle.h
@@ -5,4 +5,18 @@
 
 @interface NSWindow (KKSwizzle)
 
++ (void) load;
+
+// called when the user switches tabs (or load files)
+- (void)xxx_setRepresentedFilename:(NSString*)aPath;
+
+// called when a window is focused
+- (void)xxx_becomeMainWindow;
+
+// called when a key is pressed
+- (void)xxx_sendEvent:(NSEvent *)event;
+
+// called when a document change state (e.g. when saved to disk)
+- (void)xxx_setDocumentEdited:(BOOL)flag;
+
 @end
