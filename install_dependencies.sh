8050c9aaceb57f1b9990f2c9c61f720a0635a8e1
Alan Hamlett <alan.hamlett@gmail.com>
2016-02-13 11:07:05 -0800

    remove old wakatime-master folder before downloading from github

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -12,11 +12,15 @@ set -e
 set -x
 
 url="https://codeload.github.com/wakatime/wakatime/zip/master"
-install_dir="$HOME/Library/Application Support/TextMate/PlugIns/WakaTime.tmplugin/Contents/Resources"
-zip_file="$install_dir/wakatime-master.zip"
-installed_package="$install_dir/wakatime-master"
+extract_to="$HOME/Library/Application Support/TextMate/PlugIns/WakaTime.tmplugin/Contents/Resources"
+zip_file="$extract_to/wakatime.zip"
+installed_package="$extract_to/wakatime-master"
 
-cd "$install_dir"
+if [ -d "$installed_package" ]; then
+    rm -rf "$installed_package"
+fi
+
+cd "$extract_to"
 
 echo "Downloading wakatime package to $zip_file ..."
 curl "$url" -o "$zip_file"
@@ -26,4 +30,4 @@ unzip -o "$zip_file"
 
 rm "$zip_file"
 
-echo "Finished installing wakatime cli."
+echo "Finished."
